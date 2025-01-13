69b2113cf8f5747739f12fd5a3faa5a4d066e1f5
PingHungC <pinghung_chen@asus.com>
2025-01-13 11:17:15 +0800

    [DIT][GAI][HW1] Homework version: 0.1

--- student_assignment.py
+++ student_assignment.py
@@ -4,13 +4,66 @@ import traceback
 from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
-    pass
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature']
+    )
+
+    message__ = HumanMessage(
+            content=[
+                {"type": "text", "text": question},
+            ]
+    )
+
+    examples = [
+        {
+            "input": "2024年台灣10月紀念日有哪些?",
+            "output": """
+            "Result": [
+                {
+                    "date": "2024-10-10",
+                    "name": "國慶日"
+                }
+            ]
+            """
+        }
+    ]
+
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+        ]
+    )
+
+    few_shot_prompt  = FewShotChatMessagePromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt
+    )
+
+    final_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "請根據問題列出台灣的紀念日，以 JSON 格式輸出"),
+            few_shot_prompt,
+            ("human", "{input}"),
+        ]
+    )
+
+    chain = final_prompt | llm 
+    response = chain.invoke(input=message__)
+    return response
+
     
 def generate_hw02(question):
     pass
@@ -35,6 +88,7 @@ def demo(question):
                 {"type": "text", "text": question},
             ]
     )
+      
     response = llm.invoke([message])
     
     return response
